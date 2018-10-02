dae8e4f89050613726158d2c277f2a5e4cb295da
wintus <yuya0726+GitHub@gmail.com>
2018-10-03 00:50:23 +0900

    Implement winner & game over

--- src/tic-tac-toe/Board.tsx
+++ src/tic-tac-toe/Board.tsx
@@ -16,12 +16,15 @@ export class Board extends React.Component<any, IState> {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
-      xIsNext: true
+      xIsNext: true // X first
     };
   }
 
   render() {
-    const status = `Next player: ${this.nextValue()}`;
+    const winner = calculateWinner(this.state.squares);
+    const status = winner
+      ? `Winner: ${winner}`
+      : `Next player: ${this.nextValue()}`;
 
     return (
       <div>
@@ -37,6 +40,12 @@ export class Board extends React.Component<any, IState> {
 
   protected handleClick = (i: number) => () => {
     const squares = [...this.state.squares]; // copy
+
+    // guard: filled or game-over
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+
     squares[i] = this.nextValue();
     this.setState({ squares, xIsNext: !this.state.xIsNext });
   };
@@ -44,4 +53,25 @@ export class Board extends React.Component<any, IState> {
   protected nextValue(): Value {
     return this.state.xIsNext ? "X" : "O";
   }
+}
+
+function calculateWinner(squares: Value[]): Value {
+  const lines = [
+    // -
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // |
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // x
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  const found = lines
+    .map(([a, b, c]) => [squares[a], squares[b], squares[c]])
+    .filter(vs => vs.some(v => v !== null)) // non-null
+    .find(vs => vs.every(v => v === vs[0])); // all same
+  return found ? found[0] : null;
 }
