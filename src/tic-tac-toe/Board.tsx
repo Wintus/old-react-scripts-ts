932ab9531617ab73f522d0eafffc6eff4b110c39
wintus <yuya0726+GitHub@gmail.com>
2018-10-03 02:47:19 +0900

    simplify

--- src/tic-tac-toe/Board.tsx
+++ src/tic-tac-toe/Board.tsx
@@ -5,13 +5,12 @@ import { OX, Square } from "./Square";
 
 export type Squares = [OX, OX, OX, OX, OX, OX, OX, OX, OX]; // x9
 
-export interface IBoard {
+export interface IBoard extends IGameOver {
   squares: Squares;
+  handleClick: (i: number) => OnClick;
 }
 
-export class Board extends React.Component<
-  IBoard & { handleClick: (i: number) => OnClick } & IGameOver
-> {
+export class Board extends React.Component<IBoard> {
   shouldComponentUpdate() {
     return !this.props.gameOver;
   }
