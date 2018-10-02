225361ee5b1bed41f27eadcca43c603dd079e2f5
wintus <yuya0726+GitHub@gmail.com>
2018-10-02 23:16:05 +0900

    Add button interaction

--- src/tic-tac-toe/Square.tsx
+++ src/tic-tac-toe/Square.tsx
@@ -1,7 +1,20 @@
 import * as React from "react";
 
-export class Square extends React.Component<{ value: number }> {
-  render() {
-    return <button className="square">{this.props.value}</button>;
+export class Square extends React.Component<{}, { value: "X" | "O" | null }> {
+  constructor(props: Readonly<{}>) {
+    super(props);
+    this.state = {
+      value: null
+    };
   }
+
+  render() {
+    return (
+      <button className="square" onClick={this.setX}>
+        {this.state.value}
+      </button>
+    );
+  }
+
+  private setX = () => this.setState({ value: "X" });
 }
